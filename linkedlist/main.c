4536ab079a79ce23c1c54b2ab9d7f2ed6cf8e9c4
0x80-kernel <mauw0@protonmail.com>
2025-06-09 01:57:54 -0300

    Added comments

--- linkedlist/main.c
+++ linkedlist/main.c
@@ -210,17 +210,17 @@ void freeList(struct Head *head)
 int main()
 {
   struct Head *head = initializeList();
-  pushAtbeginning(head, 2);                              // Add 2
-  pushAtbeginning(head, 5);                              // Add 5
-  pushAtbeginning(head, 3);                              // Add 3
-  pushAtEnd(head, 10);                                   // Add 10 and the end
-  printList(head);                                       // 3 5 2 10
-  modifyValue(head, 100, 1);                             // 100 5 2 10
-  pushAtPos(head, 99, 2);                                // 100 99 5 2 10
-  printList(head);                                       // 100 99 5 2 10
-  if (contains(head, 10)) printf("True\n");              // True
-  deleteValueFromPos(head, 4);                           // 100 99 5 10
-  printList(head);                                       // 100 99 5 10
+  pushAtbeginning(head, 2);                    // Add 2
+  pushAtbeginning(head, 5);                    // Add 5
+  pushAtbeginning(head, 3);                    // Add 3
+  pushAtEnd(head, 10);                         // Add 10 and the end
+  printList(head);                             // 3 5 2 10
+  modifyValue(head, 100, 1);                   // 100 5 2 10
+  pushAtPos(head, 99, 2);                      // 100 99 5 2 10
+  printList(head);                             // 100 99 5 2 10
+  if (contains(head, 10)) printf("True\n");    // True
+  deleteValueFromPos(head, 4);                 // 100 99 5 10
+  printList(head);                             // 100 99 5 10
   freeList(head);
   return 0;
 }
